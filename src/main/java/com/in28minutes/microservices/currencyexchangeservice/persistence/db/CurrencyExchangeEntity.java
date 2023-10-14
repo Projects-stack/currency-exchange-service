4782fbcfe7c25a72d1ce600e114c0598e0c52c43
Sanket Mhetre <sanket.mhetre@neptune-ubi.com>
2023-10-14 22:08:31 +0530

    [feat]: added persistence annotations to currency-exchange-entity

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/persistence/db/CurrencyExchangeEntity.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/persistence/db/CurrencyExchangeEntity.java
@@ -1,6 +1,12 @@
 package com.in28minutes.microservices.currencyexchangeservice.persistence.db;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,17 +14,29 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "currency_exchange")
 public class CurrencyExchangeEntity
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
+    @Column(name = "conversionMultiple")
     private BigDecimal conversionMultiple;
+
+    @Transient
     private String environment;
 
-    public CurrencyExchangeEntity(Long id, String from, String to, BigDecimal conversionMultiple)
+    public CurrencyExchangeEntity(String from, String to, BigDecimal conversionMultiple)
     {
-        this.id = id;
         this.from = from;
         this.to = to;
         this.conversionMultiple = conversionMultiple;
