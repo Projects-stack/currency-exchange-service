06ee62d3b310f87e4098c70e922bede8a0552284
Sanket Mhetre <sanket.mhetre@neptune-ubi.com>
2023-10-14 22:08:45 +0530

    [feat]: added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple)
+VALUES (1000, 'USD', 'INR', 65);
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple)
+VALUES (1001, 'EUR', 'INR', 75);
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple)
+VALUES (1002, 'AUD', 'INR', 25);
