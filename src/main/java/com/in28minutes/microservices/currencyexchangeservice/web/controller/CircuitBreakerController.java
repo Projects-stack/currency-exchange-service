a80b7529db5f3910897a622eba30889056047f7c
Sanket Mhetre <sanketmhetre66@gmail.com>
2023-10-19 22:53:31 +0530

    [feat]: explored retry, circuit-breaker, rate-limiter and bulkhead options of resilience4j

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/web/controller/CircuitBreakerController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/web/controller/CircuitBreakerController.java
@@ -2,6 +2,9 @@ package com.in28minutes.microservices.currencyexchangeservice.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 import org.springframework.http.ResponseEntity;
@@ -14,7 +17,10 @@ import org.springframework.web.client.RestTemplate;
 public class CircuitBreakerController
 {
     @GetMapping("/sampleApi")
-    @Retry(name = "sampleApi", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sampleApi", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String sample()
     {
         log.info("Sample API call received");
